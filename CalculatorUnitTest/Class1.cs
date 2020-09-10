8acf8fb7f8b40f808bce86c2857a64f3b35b37bd
Mikkel Høj Hansen <mikkelhoejhansen@gmail.com>
2020-09-10 13:37:44 +0200

    added more tests

--- CalculatorUnitTest/Class1.cs
+++ CalculatorUnitTest/Class1.cs
@@ -6,8 +6,11 @@ namespace Calculatorr
     [TestFixture]
     public class TestCalculator
     {
-        [Test]
-        public void Add_IsCorrect()
+        [TestCase(10,10, 20)]
+        [TestCase(-5, 35, 30)]
+        [TestCase(0.22, -2, -1.78)]
+
+        public void Add_IsCorrect(double value, double value2, double result)
         {
             // arrange
             var uut = new Calculator();
@@ -17,11 +20,14 @@ namespace Calculatorr
             //uut.Add(7, 17);
 
             //Assert
-            Assert.That(uut.Add(7,17), Is.EqualTo((24)));
+            Assert.That(uut.Add(value,value2), Is.EqualTo((result)));
         }
 
-        [Test]
-        public void Sub_IsCorrect()
+
+        [TestCase(10, 10, 0)]
+        [TestCase(-5, 35, -40)]
+        [TestCase(0.22, -2, 2.22)]
+        public void Sub_IsCorrect(double value, double value2, double result)
         {
             // arrange
             var uut = new Calculator();
@@ -31,11 +37,13 @@ namespace Calculatorr
             //
 
             //Assert
-            Assert.That(uut.Subtract(7, 17), Is.EqualTo((-10)));
+            Assert.That(uut.Subtract(value, value2), Is.EqualTo((result)));
         }
 
-        [Test]
-        public void Mul_IsCorrect()
+        [TestCase(10, 10, 100)]
+        [TestCase(7, -5,-35)]
+        [TestCase(0.25, 10, 2.5)]
+        public void Mul_IsCorrect(double value, double value2, double result)
         {
             // arrange
             var uut = new Calculator();
@@ -45,11 +53,13 @@ namespace Calculatorr
             //
 
             //Assert
-            Assert.That(uut.Multiply(7, 17), Is.EqualTo((119)));
+            Assert.That(uut.Multiply(value, value2), Is.EqualTo((result)));
         }
 
-        [Test]
-        public void Pow_IsCorrect()
+        [TestCase(10, 2, 100)]
+        [TestCase(-5, 2, 25)]
+        [TestCase(1.5, 2, 2.25)]
+        public void Pow_IsCorrect(double value, double value2, double result)
         {
             // arrange
             var uut = new Calculator();
@@ -59,7 +69,7 @@ namespace Calculatorr
             //
 
             //Assert
-            Assert.That(uut.Power(10, 2), Is.EqualTo((100)));
+            Assert.That(uut.Power(value, value2), Is.EqualTo((result)));
         }
     }
 }
